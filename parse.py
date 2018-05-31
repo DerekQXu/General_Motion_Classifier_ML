466c246ec908d2fe16a1687a861f6d497e0fed1c
Derek Xu <derekqxu@gmail.com>
2018-05-31 09:46:39 -0700

    Update parse.py
    ummmm...

--- parse.py
+++ parse.py
@@ -7,24 +7,23 @@ testing_y = []
 filepath = 'sample.csv'
 
 with open(filepath) as fp:
-  // iterate past first line
+  # iterate past first line
   fp.readline()
   
-  // collect training data
+  # collect training data
   line_raw = fp.readline()
   while line_raw and line_raw.isdigit():
-    // collect output (n x 1)
+    # collect output (n x 1)
     line = map(int,line_raw[:len(line)-1].split(',',256))
     training_y.append(line)
-    // collect input (3 x SAMPLE_SIZE)
-    data_raw = []
+    # data_raw = []
     data_scaled_up = []
     data_scaled_down = []
     for i in range(TOTAL_SIZE):
       line_raw = fp.readline()
       line = map(int,line_raw[:len(line)-1].split(',',256))
       data_raw.append(line)
-      // data synthesis
+      # data synthesis
       data_scaled_up.append(map(lambda x: x*1.2, line))
       data_scaled_down.append(map(lambda x: x*0.8, line))
     for i in range (TOTAL_SIZE-SAMPLE_SIZE):
@@ -33,13 +32,13 @@ with open(filepath) as fp:
       training_x.append(data_scaled_down[i:SAMPLE_SIZE+i])
     line_raw = fp.readline()
 
-  // collect testing data
+  # collect testing data
   line_raw = fp.readline()
   while line:
-    // collect output (n x 1)
+    # collect output (n x 1)
     line = map(int,line_raw[:len(line)-1].split(',',256))
     testing_y.append(line)
-    // collect input (3 x SAMPLE_SIZE)
+    # collect input (3 x SAMPLE_SIZE)
     data_raw = []
     for i in range(SAMPLE_SIZE):
       line_raw = fp.readline()
